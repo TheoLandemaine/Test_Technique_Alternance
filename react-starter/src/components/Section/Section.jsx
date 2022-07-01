e32f105c71f30d532d98bb905930ec890df1c530
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-07-01 11:31:28 +0200

    save unique number from json in array

--- react-starter/src/components/Section/Section.jsx
+++ react-starter/src/components/Section/Section.jsx
@@ -30,6 +30,38 @@ class Section extends React.Component {
 
         return unique;
     }
+
+    componentDidMount = () => {
+       
+        const JsonData = UsersData;
+        let tab = []
+
+        {JsonData.map(mapedData => {
+
+            return (
+                
+                mapedData.departments.map(item => {
+                    
+                    // console.log(item)
+                    tab.push(item)
+                    tab = [...new Set(tab)];
+
+                    let test = tab.value
+                    console.log(test)
+                    // return(
+                    //     <option value={tab}>
+                    //         {tab}
+                    //     </option>
+                    // )
+
+                })
+                
+            )
+            
+        })}
+        console.log(tab)
+        
+    }
   
     render() {
 
@@ -46,27 +78,32 @@ class Section extends React.Component {
                         Nombre des départements :
                         <select >
                             <option value="Select number"> -- Sélectionné le département -- </option>
-                            {JsonData.map(mapedData => {
+                            
+                                    <option value={this.props.tab}>
+                                        {this.props.tab}
+                                    </option>
+                               
+                            
+                            {/* {JsonData.map(mapedData => {
 
-                                console.log(mapedData.departments)
-                                const uniqueNumber = this.getUnique(mapedData.departments, '')
-                                console.log(uniqueNumber)
+                                // console.log(mapedData.departments)
+                                // const uniqueNumber = this.getUnique(mapedData.departments, '')
+                                // console.log(uniqueNumber)
 
                                 return (
                                     
-                                    uniqueNumber.map(item => {
-                                        console.log(item)
+                                    mapedData.departments.map(item => {
                                         
-                                            return(
-                                                <option value={item}>
-                                                    {item}
-                                                </option>
-                                            )
-                                        
-                                        
+                                        // console.log(item)
+                                        return(
+                                            <option value={item}>
+                                                {item}
+                                            </option>
+                                        )
+
                                     })
                                 )
-                            })}
+                            })} */}
                         </select>
                     </label>
                 </form>
@@ -76,3 +113,27 @@ class Section extends React.Component {
   }
 export default Section;
 
+
+// {JsonData.map(mapedData => {
+
+//     // console.log(mapedData.departments)
+//     // const uniqueNumber = this.getUnique(mapedData.departments, '')
+//     // console.log(uniqueNumber)
+
+    
+
+//             // console.log(mapedData.departments)
+  
+//                 // tab.push(mapedData.departments);
+//                 tab2.push(mapedData.departments)
+//                 // console.log(tab)
+            
+//             console.log("test")
+//             console.log(tab2)
+//             return(
+//                 <option value={tab2}>
+//                     {tab2}
+//                 </option>
+//             )
+        
+// })}
